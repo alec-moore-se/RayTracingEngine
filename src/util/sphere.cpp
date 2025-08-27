c85afd794920fecea025dc4b3d238366de810708
Alec Moore <alec-moore-se@gmail.com>
2025-08-27 17:10:41 -0500

    optimized hit_sphere

--- src/util/sphere.cpp
+++ src/util/sphere.cpp
@@ -3,13 +3,12 @@
 double hit_sphere(const point3 &center, double radius, const ray &r) {
   vec3 oc = center - r.origin();
   double a = dot_product(r.direction(), r.direction());
-  double b = -2.0 * dot_product(r.direction(), oc);
+  double h = dot_product(r.direction(), oc);
   double c = dot_product(oc, oc) - (radius * radius);
-  double discriminant = b * b - 4 * a * c;
+  double discriminant = h * h - a * c;
 
   if (discriminant < 0)
     return -1.0;
-  else 
-    return (-b - std::sqrt(discriminant)) / (2.0 * a);
-
+  else
+    return (h - std::sqrt(discriminant)) / a;
 }
